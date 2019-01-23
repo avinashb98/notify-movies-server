0049205c3f60564b404aff104818d1d9e315eaaf
Avinash <avinashb97@gmail.com>
2019-01-23 05:54:42 +0530

    Admin Controller setup, adds signup controller

---
+++ src/controllers/admin.js
@@ -0,0 +1,30 @@
+const Admin = require('../models/adminModel');
+
+const signUp = async (req, res) => {
+  const { email, password, name } = req.body;
+  const adminData = { email };
+  if (name) adminData.name = name;
+
+  let newAdmin;
+  try {
+    newAdmin = await Admin.create(adminData);
+    await newAdmin.setPassword(password);
+  } catch (error) {
+    res.status(500).json({
+      message: 'Internal Server Error',
+      data: {}
+    });
+  }
+
+  res.status(201).json({
+    message: 'New Admin Successfuly Created',
+    data: {
+      name: newAdmin.name,
+      email: newAdmin.email
+    }
+  });
+};
+
+module.exports = {
+  signUp
+};

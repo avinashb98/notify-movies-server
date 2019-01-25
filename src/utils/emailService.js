79d538f7182e9cc78e016764cc5ab194d551a637
Avinash <avinashb97@gmail.com>
2019-01-26 02:43:51 +0530

    Adds Email Service

---
+++ src/utils/emailService.js
@@ -0,0 +1,28 @@
+const nodemailer = require('nodemailer');
+
+const transporter = nodemailer.createTransport({
+  service: 'gmail',
+  auth: {
+    user: process.env.EMAIL_ID,
+    pass: process.env.EMAIL_PASSWORD
+  }
+});
+
+const sendMail = async (emailData) => {
+  const mailOptions = {
+    from: process.env.EMAIL_ID,
+    to: emailData.recipient,
+    subject: 'Movie Details',
+    html: '<p>Inception is here</p>'
+  };
+
+  transporter.sendMail(mailOptions, (err, info) => {
+    if (err) {
+      console.log(err);
+    } else {
+      console.log(info);
+    }
+  });
+};
+
+module.exports = sendMail;

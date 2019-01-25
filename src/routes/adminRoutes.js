19636094cf1cc730f930b3dc23b6a1497c1d5f20
Avinash <avinashb97@gmail.com>
2019-01-26 01:09:05 +0530

    Adds Verify token middleware

--- src/routes/adminRoutes.js
+++ src/routes/adminRoutes.js
@@ -1,12 +1,19 @@
 const express = require('express');
 const admin = require('../controllers/admin');
 const validate = require('../controllers/validators/adminValidator');
+const verifyToken = require('../middlewares/verifyToken');
 
 const router = express.Router();
 
 router.post('/', validate.signUp, admin.signUp);
 router.post('/login', validate.login, admin.login);
 
+// Verify JWT Token
+router.use(verifyToken);
+
+// Protected Routes
+
+// Create User
 router.post('/createUser', admin.createUser);
 
 module.exports = router;
